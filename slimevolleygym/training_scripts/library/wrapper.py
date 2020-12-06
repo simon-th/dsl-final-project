79c657ffd4b6ad7da50fc7edaf59893957a44e53
sammychien <sammychien@utexas.edu>
2020-12-06 02:10:01 -0600

    Changed spike activation to be more precise

--- slimevolleygym/training_scripts/library/wrapper.py
+++ slimevolleygym/training_scripts/library/wrapper.py
@@ -1,23 +1,76 @@
 import gym
 
+RESTING_HEIGHT = 0.15
+AGENT_BALL_EUCLID_DIST = 0.044
+AGENT_MAX_LEFT_POS = 0.2
+BALL_MAX_VELOCITY = -2
+SPIKE_REWARD = 0.3
+
 class SpikeWrapper(gym.RewardWrapper):
-  def __init__(self, env):
+  def __init__(self, env, printSpikes = False):
     super(SpikeWrapper, self).__init__(env)
-    self.criteria = {
-      "Height": 1.5,
-      "HorizontalSpeed": -1.3,
-    }
+    self.pastObservation = [0 for _ in range(12)]
+    self.numSpikes = 0
+    self.printSpikes = printSpikes
+
+  def satisfiedRewardCondition(self, obs):
+    xAgent, yAgent, xVelAgent, yVelAgent, xBall, yBall, xVelBall, yVelBall = obs[0:8]
+    if self.ballOnAgentSide(xBall) and \
+       self.ballCollidedWithAgent(obs) and \
+       self.agentJumping(yAgent) and \
+       self.agentMovingLeftOrTouchingFence(xAgent, xVelAgent) and \
+       self.ballMovingFastEnoughLeft(xVelBall):
+      return True
+    else:
+      return False
+
+  def ballOnAgentSide(self, xBall):
+    return xBall > 0
+
+  def ballCollidedWithAgent(self, obs):
+    pastXVelBall = self.pastObservation[6]
+    pastYVelBall = self.pastObservation[7]
+    xAgent, yAgent = obs[0:2]
+    xBall, yBall, currXVelBall, currYVelBall = obs[4:8]
+
+    # Ball has collided with agent if the ball's y velocity goes from neg to pos
+    if (pastYVelBall < 0 and currYVelBall >= 0) or \
+       ((pastXVelBall != currXVelBall) and getEuclidDistSquared(xAgent, yAgent, xBall, yBall) < AGENT_BALL_EUCLID_DIST):
+      return True
+    else:
+      return False
+
+  def agentJumping(self, yAgent):
+    return yAgent > (RESTING_HEIGHT+0.05)
+
+  def agentMovingLeftOrTouchingFence(self, xAgent, xVelAgent):
+    return xAgent == AGENT_MAX_LEFT_POS or xVelAgent < 0
+
+  def ballMovingFastEnoughLeft(self, xVelBall):
+    return xVelBall < BALL_MAX_VELOCITY
+
+  def testCollision(self, obs):
+    xAgent, yAgent, xVelAgent, yVelAgent, xBall, yBall, xVelBall, yVelBall = obs[0:8]
+    if self.ballCollidedWithAgent(obs) and self.ballOnAgentSide(xBall):
+      self.i += 1
+      print("Ball Collided With Agent", self.i, getEuclidDistSquared(xAgent, yAgent, xBall, yBall))
 
   def step(self, action, otherAction=None):
     observation, reward, done, info = self.env.step(action, otherAction)
-    checkedreward = self.checkreward(observation, reward)
-    return observation, checkedreward, done, info
+    updatedReward = self.checkForUpdatedReward(observation, reward)
+    self.pastObservation = observation
+    return observation, updatedReward, done, info
 
-  def checkreward(self, observation, reward):
-    xPosBall, yPosBall, xVelBall, yVelBall = observation[4:8]
-    if (yPosBall > self.criteria["Height"] and xVelBall < self.criteria["HorizontalSpeed"]):
+  def checkForUpdatedReward(self, observation, reward):
+    if self.satisfiedRewardCondition(observation):
+      self.numSpikes += 1
+      if self.printSpikes:
+        print("Spike!", self.numSpikes)
       reward = self.reward(reward)
     return reward
 
   def reward(self, reward):
-    return reward + 0.4
+    return reward + SPIKE_REWARD
+
+def getEuclidDistSquared(xAgent, yAgent, xBall, yBall):
+  return (xAgent - xBall)**2 + (yAgent - yBall)**2
